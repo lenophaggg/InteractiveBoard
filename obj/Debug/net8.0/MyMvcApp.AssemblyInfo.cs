90379d0284c85e45d04ef15f701f76a0a936cf45
Андрей Ренжин <renzhinan2001@gmail.com>
2024-09-13 16:52:54 +0300

    dfkrfddmkdfvklm,;3

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9553737f21a7e2a8bf0df491654329c379fe7863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe3de68cc19d1e024f8a9e12bc9a49260653fd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
